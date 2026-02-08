c363d30785b9956eac8fe4b110bde2add409e29c
proyecto-minimarket <yguaca@gmail.com>
2026-02-07 20:57:19 -0500

    otro

--- admin/hora_test.php
+++ admin/hora_test.php
@@ -1,4 +1,7 @@
 <?php
+
+error_reporting(E_ALL); ini_set('display_errors', 1); require_once __DIR__ . '/db.php';
+
 require_once __DIR__ . '/db.php';
 
 echo "Hora PHP: " . date("Y-m-d H:i:s") . "<br>";
