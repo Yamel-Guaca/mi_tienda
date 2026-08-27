8bc6abfbc09372d3138b3c9a9f49300da6bfc075
proyecto-minimarket <yguaca@gmail.com>
2026-08-27 09:23:40 -0500

    arreglo

--- includes/config.php
+++ includes/config.php
@@ -6,13 +6,8 @@ date_default_timezone_set("America/Bogota");
 
 // Datos de conexión a la base de datos
 $servername = "localhost"; // o el host que te muestre Hostinger
-<<<<<<< HEAD
-$username   = "root";   // Usuario MySQL
-$password   = "";           // ⚠️ tu contraseña real
-=======
 $username   = "u755147454_mitienda";   // Usuario MySQL
 $password   = "N/vPLnBU@A2";           // ⚠️ tu contraseña real
->>>>>>> 1956c45c64ed310ed3aebaa10b6ead872a8da3a5
 $database   = "u755147454_mitiendabd"; // Nombre de la base de datos
 
 $app_version = "v1.0.0"; // ✅ aquí defines tu versión
